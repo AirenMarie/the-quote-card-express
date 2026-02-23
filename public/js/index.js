bd0fd6ebb0bbdea6c9a5dc7d33416ee8dd4c3c0b
AirenMarie <astringer92@gmail.com>
2026-02-22 22:58:54 -0500

    comment out getRandomImage

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ const elements = {
   author: document.getElementById("author"),
 };
 
-async function getRandomImage() {
+/* async function getRandomImage() {
   const client_id = "CLIENT_ID";
   const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
   try {
@@ -20,7 +20,7 @@ async function getRandomImage() {
   }
 }
 
-getRandomImage();
+getRandomImage(); */
 
 /* const quotes = [
   {

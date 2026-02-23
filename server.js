9793be49af1451fff1bf6cde31d0c776e6b3c2d1
AirenMarie <astringer92@gmail.com>
2026-02-22 23:16:53 -0500

    add and edit getRandomImage

--- server.js
+++ server.js
@@ -13,12 +13,27 @@ app.use(cors());
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/api/v1/getRandomImage", (request, response) => {
+
+async function getRandomImage() {
+  const endpoint = `https://api.unsplash.com/photos/random/?client_id=${process.env.CLIENT_ID}`;
+  try {
+    const response = await fetch(endpoint);
+    const returnedData = await response.json();
+    const receivedPhotoUrl = returnedData.urls.regular;
+
+    return receivedPhotoUrl;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+app.use("/api/v1/getRandomImage", async (request, response) => {
   response.status(200).json({
     status: 200,
-    data: process.env.CLIENT_ID,
+    data: await getRandomImage(),
   });
 });
+
 app.listen(port, () => {
   console.log(`Server is running http://localhost:${port}`);
   console.log("Press Ctrl+C/Cmd+C to end this process.");
